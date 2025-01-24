b397e714b23c0df04e4f7ae36a35ceb0981401f3
Sarkhail <sarkhailabbas@gmail.com>
2025-01-25 01:16:25 +0530

    Connect to remote database (Atlas).

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-function connectToDB() {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Connected to Database");
-        
-    })
-}
+dotenv.config();
 
-
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } 
+    catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
 module.exports = connectToDB;
